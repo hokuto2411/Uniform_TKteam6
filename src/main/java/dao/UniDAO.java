edd014449f996a2f68628d86b11f4e8aed938118
kosei357 <kapchi2003@gmail.com>
2026-06-19 14:20:56 +0900

    Update UniDAO.java

--- src/main/java/dao/UniDAO.java
+++ src/main/java/dao/UniDAO.java
@@ -2,8 +2,10 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import bean.Uni;
 
@@ -12,7 +14,7 @@ public class UniDAO {
 	private static String URL = "jdbc:mysql://localhost/myunidb";
 	private static String USER="root";
 	private static String PASSWD="root123";
-	
+
 	private static Connection getConnection() {
 		Connection con = null;
 		try {
@@ -23,7 +25,8 @@ public class UniDAO {
 			throw new IllegalStateException(e);
 		}
 	}
-	
+
+	//データベースにユニフォームデータを登録するメソッド
 	public void insert(Uni uni) {
 		Connection con = null;
 		Statement smt = null;
@@ -61,6 +64,133 @@ public class UniDAO {
 			}
 		}
 	}
-	
-	
-}
+
+	//全ユニフォーム情報を取得するメソッド
+	public ArrayList<Uni> selectAll(){
+		Connection con = null;
+		Statement smt = null;
+
+		ArrayList<Uni> uniList = new ArrayList<Uni>();
+
+		try {
+			String sql = "SELECT unino,uniname,price,stock,image,unifrag,uniupdatetime FROM uni ORDER BY unino";
+
+			con = getConnection();
+			smt = con.createStatement();
+			ResultSet rs = smt.executeQuery(sql);
+
+			while(rs.next()) {
+				Uni uniInfo = new Uni();
+				uniInfo.setUnino(rs.getInt("unino"));
+				uniInfo.setUniname(rs.getString("uniname"));
+				uniInfo.setPrice(rs.getInt("price"));
+				uniInfo.setStock(rs.getInt("stock"));
+				uniInfo.setImage(rs.getString("image"));
+				uniInfo.setUnifrag(rs.getInt("unifrag"));
+				uniInfo.setUniupdatetime(rs.getInt("uniupdatetime"));			
+				uniList.add(uniInfo);
+			}
+
+		}catch(Exception e) {
+			throw new IllegalStateException(e);
+		}finally{
+			if( smt != null ){
+				try{smt.close();}catch(SQLException ignore){}
+			}
+			if( con != null ){
+				try{con.close();}catch(SQLException ignore){}
+			}
+		}
+		return uniList;
+	}
+
+	//指定されたユニフォームデータを検索するメソッド
+	public Uni selectByunino(int unino){
+		Connection con = null;
+		Statement smt = null;
+
+		Uni uniInfo = new Uni();
+
+		try{
+			String sql = "SELECT unino,uniname,price,stock,image,unifrag,uniupdatetime FROM uni WHERE unino = '" + unino + "'";
+
+			con = getConnection();
+			smt = con.createStatement();
+			ResultSet rs = smt.executeQuery(sql);
+
+			while(rs.next()) {
+				uniInfo.setUnino(rs.getInt("unino"));
+				uniInfo.setUniname(rs.getString("uniname"));
+				uniInfo.setPrice(rs.getInt("price"));
+				uniInfo.setStock(rs.getInt("stock"));
+				uniInfo.setImage(rs.getString("image"));
+				uniInfo.setUnifrag(rs.getInt("unifrag"));
+				uniInfo.setUniupdatetime(rs.getInt("uniupdatetime"));			
+			}
+
+		}catch(Exception e){
+			throw new IllegalStateException(e);
+		}finally{
+			if( smt != null ){
+				try{smt.close();}catch(SQLException ignore){}
+			}
+			if( con != null ){
+				try{con.close();}catch(SQLException ignore){}
+			}
+		}
+		return uniInfo;
+	}
+
+	//指定されたユニフォームデータを削除するメソッド
+	public void delete(int unino){
+		Connection con = null;
+		Statement smt = null;
+
+		try{
+			String sql = "DELETE FROM uni WHERE unino = '"+unino+"'";
+
+			con = getConnection();
+			smt = con.createStatement();
+			smt.executeUpdate(sql);
+
+		}catch(Exception e){
+			throw new IllegalStateException(e);
+		}finally{
+			if( smt != null ){
+				try{smt.close();}catch(SQLException ignore){}
+			}
+			if( con != null ){
+				try{con.close();}catch(SQLException ignore){}
+			}
+		}
+	}
+
+	//指定されたユニフォームデータを更新するメソッド
+	public void update(Uni uni){
+
+		Connection con = null;
+		Statement smt = null;
+
+		try{
+			String sql = "UPDATE uni SET uniname='"
+					+uni.getUniname()+"',price="
+					+uni.getPrice()+" ',stock="
+					+uni.getStock()+" ',image= "
+					+uni.getImage()+" WHERE unino='"+uni.getUnino()+"'";
+
+			con = getConnection();
+			smt = con.createStatement();
+			smt.executeUpdate(sql);
+
+		}catch(Exception e){
+			throw new IllegalStateException(e);
+		}finally{
+			if( smt != null ){
+				try{smt.close();}catch(SQLException ignore){}
+			}
+			if( con != null ){
+				try{con.close();}catch(SQLException ignore){}
+			}
+		}
+	}
+
