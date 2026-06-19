91609f26dd5cd7923825a9777b28cbb2dee36d77
Liverpool0724 <t-endo@sr-net.co.jp>
2026-06-19 15:31:08 +0900

    Add files via upload

---
+++ src/main/java/servlet/Logout.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		HttpSession session=request.getSession();
+		
+		//セッションがある場合、セッションを削除
+		if(session!=null) {
+			
+			//セッション削除
+			session.invalidate();
+			
+		}
+		
+		request.getRequestDispatcher("/view/Login.jsp").forward(request, response);
+		
+	}
+}
