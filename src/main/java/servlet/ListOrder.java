ad5da204cc5abc67798e2654283ced296217dd2c
kanda-it <kanda-it@172.16.1.11>
2026-06-23 10:06:12 +0900

    add 注文履歴ユーザjava

--- src/main/java/servlet/ListOrder.java
+++ src/main/java/servlet/ListOrder.java
@@ -1,5 +1,36 @@
 package servlet;
 
-public class ListOrder {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import bean.Order;
+import dao.OrderDAO;
+
+@WebServlet("/listOrder")
+public class ListOrder extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String error = "";		
+		try {
+			OrderDAO OrderDaoObj = new OrderDAO();
+			ArrayList<Order> list = OrderDaoObj.selectAll();
+			request.setAttribute("order_list",list);			
+			
+			request.getRequestDispatcher("/view/listOrder.jsp").forward(request, response);
+			
+		} catch(IllegalStateException e) {
+			System.out.println(e);
+			error = "DB接続エラー";
+			request.setAttribute("error",error);
+			request.setAttribute("cmd","logout");
+			request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+		}
+	}
 }
