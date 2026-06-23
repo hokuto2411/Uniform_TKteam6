c821efee0ca9b82a9188798a3460b272a38d2218
kanda-it <kanda-it@172.16.1.11>
2026-06-23 11:38:03 +0900

    add 購入

--- src/main/java/servlet/BuyComfirm.java
+++ src/main/java/servlet/BuyComfirm.java
@@ -1,7 +1,7 @@
 package servlet;
 
-import java.awt.print.Book;
 import java.io.IOException;
+import java.sql.Date;
 import java.util.ArrayList;
 
 import jakarta.servlet.ServletException;
@@ -10,12 +10,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+import bean.Order;
 import bean.OrderDetail;
-import bean.Uni;
 import bean.Uniform;
 import bean.User;
 import dao.OrderDAO;
 import dao.UniformDAO;
+import util.MyFormat;
 import util.SendMail;
 
 @WebServlet("/buyConfirm")
@@ -33,7 +34,6 @@ public class BuyComfirm {
 				request.setAttribute("cmd","logout");
 				request.getRequestDispatcher("/view/error.jsp").forward(request, response);
 			}
-			
 			ArrayList<OrderDetail> detail_list = (ArrayList<OrderDetail>)session.getAttribute("detail_list");
 			if(detail_list == null) {
 				error = "カートの中に何も無かったので購入は出来ません。 ";
@@ -42,43 +42,52 @@ public class BuyComfirm {
 				request.getRequestDispatcher("/view/error.jsp").forward(request, response);
 			}
 			
+			String comment = (String)request.getAttribute("comment");
 			UniformDAO UniDaoObj = new UniformDAO();
 			OrderDAO OrderDaoObj = new OrderDAO();
-			ArrayList<Uni> uni_list = new ArrayList<Uni>();
+			MyFormat fmt = new MyFormat();
+			Date sqlDate = new Date(System.currentTimeMillis());
+			Order order = new Order();
+			ArrayList<Uniform> uni_list = new ArrayList<Uniform>();
 			String msg = ""
 					+ (String)user.getUserid()
 					+ "様\n"
 					+ "\n"
-					+ "本のご購入ありがとうございます。\n"
+					+ "ユニフォームのご購入ありがとうございます。\n"
 					+ "以下内容でご注文を受け付けましたので、ご連絡致します。\n"
 					+ "\n";
 			int total = 0;
 			
 			for(int i=0; i < detail_list.size(); i++) {
 				OrderDetail detail = detail_list.get(i);
-				Uniform uni = UniDaoObj.selectByunino(detail.getUnino());
-				// ↓???
-				OrderDaoObj.insert(detail);
-				list.add(Book);
+				Uniform uni = UniDaoObj.selectByUnino(detail.getUnino());
+				uni_list.add(uni);
+				
+				// orderSet
+				order.setOrderno(detail.getOrderno());
+				order.setUserno(user.getUserno());
+				total += (uni.getPrice() * detail.getQuantity());
+				order.setOrderdate(sqlDate);
+				order.setOrdercomment(comment);
+				
+				
 				// メール本文作成
-				total += uni.getPrice();
-				msg += uni.get()
+				msg += uni.getUniname()
 						+ " "
-						+ Book.getTitle()
+						+ detail.getQuantity()
 						+ " "
-						+ Book.getPrice()
+						+ (uni.getPrice() * detail.getQuantity())
+						+ " "
+						+ fmt.moneyFormat(uni.getPrice())
 						+ "\n";								
 			}
 			msg += ""
 					+ "合計	"
-					+ total
+					+ fmt.moneyFormat(total)
 					+ "円\n"
 					+ "\n"
 					+ "またのご利用よろしくお願いします。\n"
-					+ "";
-			
-			request.setAttribute("book_list",list);
-			
+					+ "";			
 			// メール送信
 			SendMail mail = new SendMail();
 			boolean TFMail = mail.sendMail(msg);
@@ -89,7 +98,9 @@ public class BuyComfirm {
 				request.getRequestDispatcher("/view/error.jsp").forward(request, response);
 			}
 			
-			session.setAttribute("order_list",null);
+			order.setSumprice(total);
+			OrderDaoObj.insert(order);
+			session.setAttribute("detail_list",null);
 			request.getRequestDispatcher("/view/buyConfirm.jsp").forward(request, response);
 			
 		}catch(Exception e) {
