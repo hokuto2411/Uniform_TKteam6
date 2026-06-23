0716b38a1e5742b72f541c62cb13310238c11e8a
Liverpool0724 <t-endo@sr-net.co.jp>
2026-06-23 10:05:33 +0900

    Add files via upload

---
+++ src/main/java/servlet/DeleteUser.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import bean.User;
+import dao.UserDAO;
+
+@WebServlet("/deleteUser")
+public class DeleteUser extends HttpServlet {
+	public class Login extends HttpServlet {
+		protected void doPost(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+
+			UserDAO objUserDAO = new UserDAO();
+			User objUser = new User();
+
+			String userid = request.getParameter("userid");
+			String password = request.getParameter("password");
+
+			objUser = objUserDAO.selectByUser(userid, password);
+			if (objUser.getUsername() == null) {
+				String message = "削除対象の書籍が存在しない為、書籍削除処理は行えませんでした。";
+				request.setAttribute("message", message);
+				request.getRequestDispatcher("/view/error.jsp?cmd=list").forward(request, response);
+
+			}
+			//deleteメソッドを利用して書籍情報を削除
+			try {
+
+				objUserDAO.deleteUser(objUser);
+
+			} catch (IllegalStateException e) {
+
+				String message = "DB接続エラーの為、削除は行えませんでした。";
+				request.setAttribute("message", message);
+				request.getRequestDispatcher("/view/error.jsp?cmd=logout").forward(request, response);
+
+			} finally {
+				//ログイン画面へフォワード
+				request.getRequestDispatcher("/view/login,jsp").forward(request, response);
+			}
+		}
+	}
+}
