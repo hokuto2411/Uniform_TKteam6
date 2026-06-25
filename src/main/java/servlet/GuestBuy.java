faa6dd514431ee608507887620827248979ab4b8
takashimaaa <s-takashima@sr-net.co.jp>
2026-06-25 15:48:56 +0900

    Add files via upload

---
+++ src/main/java/servlet/GuestBuy.java
@@ -0,0 +1,53 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import bean.User;
+import dao.UserDAO;
+
+
+@WebServlet("/guestBuy")
+public class GuestBuy extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String error="";
+		
+		try {
+			HttpSession session = request.getSession();
+			User user=(User)session.getAttribute("user");
+			UserDAO userdao =new UserDAO();
+			
+			
+			user.setUsername(request.getParameter("username"));
+			user.setAddress(request.getParameter("address"));
+			user.setMailaddress(request.getParameter("mailaddress"));
+			
+			userdao.guestUserUpdate(user);
+			session.setAttribute("user", user);
+			
+			request.getRequestDispatcher("/view/buyConfirm.jsp").forward(request, response);
+			
+		}catch(IllegalStateException e) {
+			System.out.println(e);
+			error = "DB接続エラーのため、カートを表示できませんでした。";
+			request.setAttribute("error",error);
+			request.setAttribute("cmd","menu");
+			request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+
+		}catch(Exception e){
+			error = "予期せぬエラーが発生しました。<br>" + e;
+			request.setAttribute("error",error);
+			request.setAttribute("cmd","menu");
+			request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+		}
+		
+		
+	}
+}
